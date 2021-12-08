55423976bc5a5eed8550f22d0272910e1e308567
patrickvodka <LeNotraldu22@gmail.com>
2021-12-08 16:11:30 +0100

    levier,levelDesign

--- Assets/Script/JumpBoostDown.cs
+++ Assets/Script/JumpBoostDown.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Onclick : MonoBehaviour, IClickable
+public class JumpBoostDown : MonoBehaviour, IClickable
 {
     public Jump jump;
     private void Start()
     {
-        jump = FindObjectOfType<Jump>();   
+        jump = FindObjectOfType<Jump>();
     }
 
     public void click()
     {
 
-        jump.jumpForce += 0.8f;
+        jump.jumpForce -= 1.6f;
         Destroy(gameObject);
     }
 }
